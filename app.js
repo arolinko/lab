42eb16a1fe52f01a84a1f723a5dd8ec37ea54ef5
arolinko <p.ararat.a@gmail.com>
2024-10-20 01:24:45 +0400

    app on port 3000

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // app.js
-console.log("Hello, World!");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server and listen on the defined port
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
